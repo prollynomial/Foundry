6d1ad5a54fd01f16850612498ca4a60695f1dd62
Adam Carruthers <sudo.adam.carruthers@gmail.com>
2011-09-22 17:33:24 -0400

    account for particles moving faster than the speed of light

--- src/com/adamcarruthers/foundry/Constants.java
+++ src/com/adamcarruthers/foundry/Constants.java
@@ -5,4 +5,7 @@ import android.view.Menu;
 public class Constants {
 	public static final int DIALOG_ADD_SOURCE_ID = 0;
 	public static final int MENU_ADD_SOURCE_ID = Menu.FIRST;
+	
+	public static final int SPEED_OF_LIGHT = 299792458;
+	public static final int SPEED_OF_NEUTRINOS = 299799953;
 }
