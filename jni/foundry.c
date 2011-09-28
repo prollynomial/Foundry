f99902de611246109590f361ab79459928f8c539
Adam Carruthers <sudo.adam.carruthers@gmail.com>
2011-09-27 20:00:07 -0400

    remove jni stuff

--- jni/foundry.c
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-#include <jni.h>
-
-jstring
-Java_com_adamcarruthers_foundry_APTActivity_lawlJNI (JNIEnv *env, jobject thiz) {
-	return (*env)->NewStringUTF(env, "Whuddup, Foundry?");
-}
